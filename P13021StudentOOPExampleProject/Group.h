f133b2d38e7a39263f83d899464ffd8f5c4e4e8b
Victor <ivanvikvik@gmail.com>
2022-04-05 18:45:49 +0300

    Add Group class with function description

---
+++ P13021StudentOOPExampleProject/Group.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Student.h"
+
+class Group
+{
+private:
+	string name;
+	int size;
+	Student* list;
+
+public:
+	Group();
+	Group(string name);
+	Group(string name, Student* list, int size);
+
+	//get/set
+
+	void add(Student student);
+	Student get(int index);
+	int getSize();
+
+	string getInfo();
+
+};
+
