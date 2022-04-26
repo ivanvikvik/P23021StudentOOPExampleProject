5c5aa79d5b911153063ab5df3e7dded5b8752faf
Victor <ivanvikvik@gmail.com>
2022-04-26 18:36:09 +0300

    Add UML Classes Diagram and Inheritance

--- P13021StudentOOPExampleProject/Leader.h
+++ P13021StudentOOPExampleProject/Leader.h
@@ -1,42 +1,20 @@
 #pragma once
 #include "Student.h"
 
-class Leader
+// дочерний класс
+// родительский класс
+
+class Leader : public Student
 {
-private:
-	static int count;
-
-	string name;
-	int age;
-	double mark;
-	char sex;
-	bool alive;
-
+private:	
 	string phoneNumber;
 	string e_mail;
 
 public:
-	static const int MAX_STUDENT_AGE = 80;
-	static const int MIN_STUDENT_AGE = 14;
-
-	static const int MAX_MARK = 10;
-	static const int MIN_MARK = 0;
-
 	Leader();
 	Leader(string name, int age, double mark, char sex, bool alive, 
 		string phoneNumber, string e_mail);
 	~Leader();
-
-	string getName();
-	void setName(string n);
-	int getAge();
-	void setAge(int a);
-	double getMark();
-	void setMark(double m);
-	char getSex();
-	void setSex(char s);
-	bool isAlive();
-	void setAlive(bool al);
 
 	string getPhoneNumber();
 	void setPhoneNumber(string phoneNumber);
@@ -44,9 +22,6 @@ public:
 	string getEmail();
 	void setEmail(string e_mail);
 
-	static int getCount();
-
 	string getInfo();
-
 };
 
--- P13021StudentOOPExampleProject/SuperStudent.h
+++ P13021StudentOOPExampleProject/SuperStudent.h
@@ -1,51 +1,24 @@
 #pragma once
 #include "Student.h"
 
-class SuperStudent
+class SuperStudent : public Student
 {
 private:
-	static int count;
-
-	string name;
-	int age;
-	double mark;
-	char sex;
-	bool alive;
-
 	string subject;
 	string ability;
 
 public:
-	static const int MAX_STUDENT_AGE = 80;
-	static const int MIN_STUDENT_AGE = 14;
-
-	static const int MAX_MARK = 10;
-	static const int MIN_MARK = 0;
-
 	SuperStudent();
 	SuperStudent(string name, int age, double mark, char sex, bool alive,
 		string subject, string ability);
 	~SuperStudent();
 
-	string getName();
-	void setName(string n);
-	int getAge();
-	void setAge(int a);
-	double getMark();
-	void setMark(double m);
-	char getSex();
-	void setSex(char s);
-	bool isAlive();
-	void setAlive(bool al);
-
 	string getSubject();
 	void setSubject(string subject);
+	
 	string getAbility();
 	void setAbility(string ability);
 
-	static int getCount();
-
 	string getInfo();
-
 };
 
